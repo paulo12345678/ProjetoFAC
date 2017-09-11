8422bcde297f00b0e88a9d3b534a1c6411643f0e
Emeson Santos <esodegemeos@gmail.com>
2017-09-11 09:49:08 -0300

    Criação do brench 11-09-2017

--- Projeto FAC/10-implementação/SistemaFac/Model/Models/Servico.cs
+++ Projeto FAC/10-implementação/SistemaFac/Model/Models/Servico.cs
@@ -1,6 +1,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 
@@ -26,13 +27,16 @@ namespace Model.Models
         }
 
         public Servico() : this(0, null) { }
-
+        [Key]
         public int Id
         {
             get { return id; }
             set { id = value; }
         }
 
+        [Required(ErrorMessage = "Obrigatorio preencher o Campo Vasio")] //Obrigatoria
+        [StringLength(45, ErrorMessage = "No maximo 45 Caracteres")] //tamanho do texto se ultrapassar aparecem a mensagem de erro
+        [Display(Name = "Nome do tipo de serviço")] // é como se fosse um label pra depois vim o campo texto se não for colocado vai aparecer o nome exato do metodo 
         public string Descricao
         {
             get { return descricao; }
