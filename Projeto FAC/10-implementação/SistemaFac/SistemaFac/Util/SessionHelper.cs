72fbfbe0bd0457ce439a4eae777bbedd622de625
Emeson Santos <esodegemeos@gmail.com>
2017-09-27 14:22:15 -0300

    VERIFICAR TEG

---
+++ Projeto FAC/10-implementação/SistemaFac/SistemaFac/Util/SessionHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaFac.Util
+{
+    public enum SessionKeys { USUARIO }
+
+    public static class SessionHelper
+    {
+        public static object Get(SessionKeys chave)
+        {
+            string chaveString = Enum.GetName(typeof(SessionKeys), chave);
+            return HttpContext.Current.Session[chaveString];
+        }
+
+        public static object Set(SessionKeys chave,object valor)
+        {
+            string chaveString = Enum.GetName(typeof(SessionKeys), chave);
+            return HttpContext.Current.Session[chaveString] = valor;
+        }
+    }
+}
